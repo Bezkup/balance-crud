8eb1f865ed36d181d3f0c8b7c841d6a6ca5a75f0
Bezkup <bruroy96@gmail.com>
2022-12-21 13:14:06 +0100

    Added class in previous commit

--- src/main/java/it/dreamteam/balance/model/request/TransactionRequest.java
+++ src/main/java/it/dreamteam/balance/model/request/TransactionRequest.java
@@ -1,11 +1,26 @@
 package it.dreamteam.balance.model.request;
 
+import it.dreamteam.balance.model.CategoryModel;
 import it.dreamteam.balance.model.TransactionModel;
+import it.dreamteam.balance.util.TransactionType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 
 @Getter
 @Setter
-public class TransactionRequest extends TransactionModel {
+@AllArgsConstructor
+public class TransactionRequest {
+    protected TransactionType transactionType;
+
+    protected int amount;
+
+    protected LocalDate transactionDate;
+
+    protected String description;
+
+    protected String category;
 }
